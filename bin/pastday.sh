f833f004925338edb1d452ff7f6492e23988d292
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-05-08 07:51:03 +0200

    graph names

--- bin/pastday.sh
+++ bin/pastday.sh
@@ -2,11 +2,13 @@
 
 HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 PASTDAY_IMAGE='/tmp/lektrix/site/img/lex_pastday.png'
+PASTDAY_ZAP_IMAGE='/tmp/lektrix/site/img/lex_pastday.png'
 
 pushd "${HERE}" >/dev/null || exit 1
 echo "no graphing defined"
 if [ ! -f ${PASTDAY_IMAGE} ]; then
     cp "${HERE}/fles/static/empty.png" ${PASTDAY_IMAGE}
+    cp "${HERE}/fles/static/empty.png" ${PASTDAY_ZAP_IMAGE}
 fi
 # ./trend.py --hours 0
 popd >/dev/null || exit
