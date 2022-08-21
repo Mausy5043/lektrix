71216ad83e4fb5e9b4ee65be327528205fa4fe48
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-08-21 11:27:48 +0200

    enable graphs

--- bin/pastyear.sh
+++ bin/pastyear.sh
@@ -6,10 +6,10 @@ HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 
 pushd "${HERE}" >/dev/null || exit 1
 
-#./trendyg.py --gauge 0 &
+./trendyg.py --gauge 0
 ./lg-trend.py --months 0
 ./lg-trend.py --years 0
-#./trendyg.py --months 0 &
+./trendyg.py --months 0
 #wait
 
 popd >/dev/null || exit
