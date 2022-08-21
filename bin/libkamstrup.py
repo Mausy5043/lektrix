5c7c4140383ef0c59260a578f47a560d76e044c6
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-08-21 10:57:16 +0200

    'energy' table now contains 15' data

--- bin/libkamstrup.py
+++ bin/libkamstrup.py
@@ -228,7 +228,7 @@ def add_time_line(config):
         final_epoch = int(dt.datetime.strptime(f"{ytf}-01-01 00:00", "%Y-%m-%d %H:%M"
                                                ).timestamp()
                           )
-    step_epoch = 10 * 60
+    step_epoch = 15 * 60
     multi = 3600
     if config["timeframe"] == "hour":
         multi = 3600
