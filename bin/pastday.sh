44c42d6546424e6f7d3f3965af2f19478a88e139
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-10-06 09:52:55 +0200

    add production trends

--- bin/pastday.sh
+++ bin/pastday.sh
@@ -3,5 +3,6 @@
 HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 
 pushd "${HERE}" >/dev/null || exit 1
+    ./ms-trend.py --hours 0
     ./lg-trend.py --hours 0
 popd >/dev/null || exit
