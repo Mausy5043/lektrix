9d73acc63b8221ca48d201ea6837eaf4755aaba8
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-10-09 09:02:27 +0200

    rotation same as x-ticks

--- bin/ms-trend.py
+++ bin/ms-trend.py
@@ -216,7 +216,7 @@ def plot_graph(output_file, data_dict, plot_title, show_data=False, locatorforma
                 for p in ax1.patches:
                     b = p.get_bbox()
                     val = "{:+.3f}".format(b.y1 - b.y0)
-                    ax1.annotate(val, ((b.x0 + b.x1) / 2 + x_offset, b.y1 + y_offset), rotation=90)
+                    ax1.annotate(val, ((b.x0 + b.x1) / 2 + x_offset, b.y1 + y_offset), rotation=30)
             ax1.set_ylabel(parameter)
             ax1.legend(loc='lower left',
                        ncol=8,
