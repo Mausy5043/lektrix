79199697da6e30b46c2d50fff7b1938a049b3a97
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-10-09 09:44:34 +0200

    cleanup

--- bin/ms-trend.py
+++ bin/ms-trend.py
@@ -6,6 +6,7 @@ Using kamstrup data
 
 # FIXME: still needs work
 import warnings
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 import argparse
@@ -53,9 +54,7 @@ def fetch_data(hours_to_fetch=48, aggregation='W'):
     df_mains['EB'] += (df_mains['T1out'] + df_mains['T2out'])  # T1out and T2out are (-)-ve values !
     # put columns in the right order for plotting
     categories = ['T1out', 'T2out', 'EB', 'T1in', 'T2in']
-    df_mains.columns = pd.CategoricalIndex(df_mains.columns.values,
-                                           ordered=True,
-                                           categories=categories)
+    df_mains.columns = pd.CategoricalIndex(df_mains.columns.values, ordered=True, categories=categories)
     df_mains = df_mains.sort_index(axis=1)
     if DEBUG:
         print(f"\n\n  ** MAINS data for plotting ** ")
@@ -90,18 +89,14 @@ def fetch_data_mains(hours_to_fetch=48, aggregation='H'):
     if DEBUG:
         print(s3_query)
     with s3.connect(DATABASE) as con:
-        df = pd.read_sql_query(s3_query,
-                               con,
-                               parse_dates='sample_time',
-                               index_col='sample_epoch'
-                               )
+        df = pd.read_sql_query(s3_query, con, parse_dates='sample_time', index_col='sample_epoch')
     if DEBUG:
         print("o  database data")
         print(df)
     for c in df.columns:
         if c not in ['sample_time']:
             df[c] = pd.to_numeric(df[c], errors='coerce')
-    df.index = pd.to_datetime(df.index, unit='s')
+    df.index = pd.to_datetime(df.index, unit='s')  # noqa
     # resample to monotonic timeline
     df = df.resample(f'{aggregation}', label='left').max()
 
@@ -138,11 +133,7 @@ def fetch_data_production(hours_to_fetch=48, aggregation='H'):
     if DEBUG:
         print(s3_query)
     with s3.connect(DATABASE) as con:
-        df = pd.read_sql_query(s3_query,
-                               con,
-                               parse_dates='sample_time',
-                               index_col='sample_epoch'
-                               )
+        df = pd.read_sql_query(s3_query, con, parse_dates='sample_time', index_col='sample_epoch')
     if DEBUG:
         print("o  database data")
         print(df)
@@ -150,7 +141,7 @@ def fetch_data_production(hours_to_fetch=48, aggregation='H'):
         if c not in ['sample_time']:
             df[c] = pd.to_numeric(df[c], errors='coerce')
     # df.index = pd.to_datetime(df.index, unit='s').tz_localize("UTC").tz_convert("Europe/Amsterdam")
-    df.index = pd.to_datetime(df.index, unit='s')
+    df.index = pd.to_datetime(df.index, unit='s')  # noqa
 
     # resample to monotonic timeline
     df = df.resample(f'{aggregation}', label='left').sum()
@@ -166,7 +157,7 @@ def fetch_data_production(hours_to_fetch=48, aggregation='H'):
     return df
 
 
-def plot_graph(output_file, data_dict, plot_title, show_data=False, locatorformat=['hour', '%d-%m %Hh']):
+def plot_graph(output_file, data_dict, plot_title, show_data=False, locatorformat=None):
     """Plot the data in a chart.
 
     Args:
@@ -175,9 +166,12 @@ def plot_graph(output_file, data_dict, plot_title, show_data=False, locatorforma
         data_dict (dict): dict containing the datasets to be plotted
         plot_title (str): text for the title to be placed above the plot
         show_data (bool): whether to show numerical values in the plot.
+        locatorformat (list): formatting information for xticks
 
     Returns: nothing
     """
+    if locatorformat is None:
+        locatorformat = ['hour', '%d-%m %Hh']
     if DEBUG:
         print("\n\n*** PLOTTING ***")
     for parameter in data_dict:
@@ -203,39 +197,28 @@ def plot_graph(output_file, data_dict, plot_title, show_data=False, locatorforma
 
             # create a line plot
             plt.rc('font', size=fig_fontsize)
-            ax1 = data_frame.plot.bar(stacked=True,
-                                      figsize=(fig_x, fig_y),
-                                      color=['skyblue', 'blue', 'seagreen', 'salmon', 'red']
-                                      )
+            ax1 = data_frame.plot(kind='bar', stacked=True, width=0.9,
+                                  figsize=(fig_x, fig_y),
+                                  color=['skyblue', 'blue', 'seagreen', 'salmon', 'red']
+                                  )
             # linewidth and alpha need to be set separately
             for i, l in enumerate(ax1.lines):
                 plt.setp(l, alpha=ahpla, linewidth=1, linestyle=' ')
             if show_data:
                 x_offset = -0.1
-                y_offset = 2
                 for p in ax1.patches:
                     b = p.get_bbox()
                     val = "{:+.3f}".format(b.y1 - b.y0)
-                    ax1.annotate(val, ((b.x0 + b.x1) / 2 + x_offset, b.y0 + 0.5*(b.y1-b.y0)), rotation=30)
+                    ax1.annotate(val, ((b.x0 + b.x1) / 2 + x_offset, b.y0 + 0.5 * (b.y1 - b.y0)), rotation=30)
             ax1.set_ylabel(parameter)
-            ax1.legend(loc='lower left',
-                       ncol=8,
-                       framealpha=0.2
-                       )
+            ax1.legend(loc='lower left', ncol=8, framealpha=0.2)
             ax1.set_xlabel("Datetime")
-            ax1.grid(which='major',
-                     axis='y',
-                     color='k',
-                     linestyle='--',
-                     linewidth=0.5
-                     )
+            ax1.grid(which='major', axis='y', color='k', linestyle='--', linewidth=0.5)
             ax1.xaxis.set_major_formatter(mticker.FixedFormatter(ticklabels))
             plt.gcf().autofmt_xdate()
             plt.title(f'{parameter} {plot_title}')
             plt.tight_layout()
-            plt.savefig(fname=f'{output_file}_{parameter}.png',
-                        format='png'
-                        )
+            plt.savefig(fname=f'{output_file}_{parameter}.png', format='png')
             if DEBUG:
                 print(f" --> {output_file}_{parameter}.png\n")
 
@@ -248,69 +231,40 @@ def main():
         aggr = 60  # int(float(OPTION.hours) * 60. / 480)
         if aggr < 1:
             aggr = 1
-        plot_graph(constants.TREND['hour_graph'],
-                   fetch_data(hours_to_fetch=OPTION.hours, aggregation='H'),
+        plot_graph(constants.TREND['hour_graph'], fetch_data(hours_to_fetch=OPTION.hours, aggregation='H'),
                    f" trend afgelopen uren ({dt.now().strftime('%d-%m-%Y %H:%M:%S')})",
-                   locatorformat=['hour','%d-%m %Hh']
-                   )
+                   locatorformat=['hour', '%d-%m %Hh'])
     if OPTION.days:
         aggr = 60 * 24  # int(float(OPTION.days) * 24. * 60. / 5760.)
         if aggr < 1:
             aggr = 1
-        plot_graph(constants.TREND['day_graph'],
-                   fetch_data(hours_to_fetch=OPTION.days * 24, aggregation='D'),
+        plot_graph(constants.TREND['day_graph'], fetch_data(hours_to_fetch=OPTION.days * 24, aggregation='D'),
                    f" trend afgelopen dagen ({dt.now().strftime('%d-%m-%Y %H:%M:%S')})",
-                   locatorformat=['day','%Y-%m-%d']
-                   )
+                   locatorformat=['day', '%Y-%m-%d'])
     if OPTION.months:
         aggr = 60 * 24 * 31  # int(float(OPTION.months) * 30.5 * 24. * 60.  / 9900.)
         if aggr < 1:
             aggr = 1
-        plot_graph(constants.TREND['month_graph'],
-                   fetch_data(hours_to_fetch=OPTION.months * 31 * 24, aggregation='M'),
-                   f" trend afgelopen maanden ({dt.now().strftime('%d-%m-%Y %H:%M:%S')})",
-                   show_data=True,
-                   locatorformat=['month','%Y-%m']
-                   )
+        plot_graph(constants.TREND['month_graph'], fetch_data(hours_to_fetch=OPTION.months * 31 * 24, aggregation='M'),
+                   f" trend afgelopen maanden ({dt.now().strftime('%d-%m-%Y %H:%M:%S')})", show_data=True,
+                   locatorformat=['month', '%Y-%m'])
     if OPTION.years:
         aggr = 24 * 60 * 366  # int(float(OPTION.years) * 366 * 24. * 60.)
         if aggr < 1:
             aggr = 1
-        plot_graph(constants.TREND['year_graph'],
-                   fetch_data(hours_to_fetch=OPTION.years * 366 * 24, aggregation='A'),
-                   f" trend afgelopen jaren ({dt.now().strftime('%d-%m-%Y %H:%M:%S')})",
-                   show_data=True,
-                   locatorformat=['year','%Y']
-                   )
+        plot_graph(constants.TREND['year_graph'], fetch_data(hours_to_fetch=OPTION.years * 366 * 24, aggregation='A'),
+                   f" trend afgelopen jaren ({dt.now().strftime('%d-%m-%Y %H:%M:%S')})", show_data=True,
+                   locatorformat=['year', '%Y'])
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create a trendgraph")
-    parser.add_argument("-hr",
-                        "--hours",
-                        type=int,
-                        help="create hour-trend for last <HOURS> hours",
-                        )
-    parser.add_argument("-d",
-                        "--days",
-                        type=int,
-                        help="create day-trend for last <DAYS> days"
-                        )
-    parser.add_argument("-m",
-                        "--months",
-                        type=int,
-                        help="number of months of data to use for the graph",
-                        )
-    parser.add_argument("-y",
-                        "--years",
-                        type=int,
-                        help="number of months of data to use for the graph",
-                        )
+    parser.add_argument("-hr", "--hours", type=int, help="create hour-trend for last <HOURS> hours", )
+    parser.add_argument("-d", "--days", type=int, help="create day-trend for last <DAYS> days")
+    parser.add_argument("-m", "--months", type=int, help="number of months of data to use for the graph", )
+    parser.add_argument("-y", "--years", type=int, help="number of months of data to use for the graph", )
     parser_group = parser.add_mutually_exclusive_group(required=False)
-    parser_group.add_argument("--debug",
-                              action="store_true",
-                              help="start in debugging mode"
-                              )
+    parser_group.add_argument("--debug", action="store_true", help="start in debugging mode")
     OPTION = parser.parse_args()
     if OPTION.hours == 0:
         OPTION.hours = 6 * 12
