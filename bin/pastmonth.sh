b098a9b648e14a28a20b96aa83021684a1254550
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-12-31 08:28:51 +0100

    align code with other projects

--- bin/pastmonth.sh
+++ bin/pastmonth.sh
@@ -11,32 +11,42 @@ source ./constants.sh
 
 if [ "${MAINTENANCE}" == "-" ]; then
     # do some maintenance
-
     CURRENT_EPOCH=$(date +'%s')
 
     # shellcheck disable=SC2154
     echo "${db_full_path} re-indexing... "
     sqlite3 "${db_full_path}" "REINDEX;"
 
-    if [ $? -eq 0 ]; then
-        echo -n "${db_full_path} integrity check:   "
-        chk_result=$(sqlite3 "${db_full_path}" "PRAGMA integrity_check;")
-        echo " ${chk_result}"
-        if [ "${chk_result}" == "ok" ]; then
-            echo "${db_full_path} copying... "
+    echo -n "${db_full_path} integrity check:   "
+    chk_result=$(sqlite3 "${db_full_path}" "PRAGMA integrity_check;")
+    echo " ${chk_result}"
+    if [ "${chk_result}" == "ok" ]; then
+        echo "${db_full_path} copying to backup... "
+        # copy to backup
+        if command -v rclone &> /dev/null; then
             # shellcheck disable=SC2154
-            cp "${db_full_path}" "${database_path}/backup/"
-
-            # Keep upto 10 years of data
-            echo "${db_full_path} vacuuming... "
-            PURGE_EPOCH=$(echo "${CURRENT_EPOCH} - (3660 * 24 * 3600)" | bc)
-            sqlite3 "${db_full_path}" \
-                "DELETE FROM mains WHERE sample_epoch < ${PURGE_EPOCH};"
-            sqlite3 "${db_full_path}" \
-                "DELETE FROM production WHERE sample_epoch < ${PURGE_EPOCH};"
-            sqlite3 "${db_full_path}" \
-                "DELETE FROM charger WHERE sample_epoch < ${PURGE_EPOCH};"
+            rclone copyto -v \
+                   "${database_local_root}/${app_name}/${database_filename}" \
+                   "${database_remote_root}/backup/${database_filename}"
         fi
+
+        # Keep upto 10 years of data
+        echo "${db_full_path} vacuuming... "
+        PURGE_EPOCH=$(echo "${CURRENT_EPOCH} - (3660 * 24 * 3600)" | bc)
+        sqlite3 "${db_full_path}" \
+            "DELETE FROM mains WHERE sample_epoch < ${PURGE_EPOCH};"
+        sqlite3 "${db_full_path}" \
+            "DELETE FROM production WHERE sample_epoch < ${PURGE_EPOCH};"
+        sqlite3 "${db_full_path}" \
+            "DELETE FROM charger WHERE sample_epoch < ${PURGE_EPOCH};"
+    fi
+    # sync the database into the cloud
+    if command -v rclone &> /dev/null; then
+        echo "${db_full_path} syncing... "
+        # shellcheck disable=SC2154
+        rclone copyto -v \
+               "${database_local_root}/${app_name}/${database_filename}" \
+               "${database_remote_root}/${app_name}/${database_filename}"
     fi
 fi
 
