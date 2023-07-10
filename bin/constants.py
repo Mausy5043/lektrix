5ebd057c05a084462d5ab41ed05a61219ac465e8
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2023-07-10 16:01:00 +0200

    change over to latest bootstrap

--- bin/constants.py
+++ bin/constants.py
@@ -58,12 +58,12 @@ TREND = {
     "year_graph": "/tmp/lektrix/site/img/lex_pastyears",
     "yg_vs_month": "/tmp/lektrix/site/img/lex_vs_month",
     "yg_gauge": "/tmp/lektrix/site/img/lex_gauge",
-    "hour_graph_v2": "/tmp/lektrix/site/test/img/lex_pasthours",
-    "day_graph_v2": "/tmp/lektrix/site/test/img/lex_pastdays",
-    "month_graph_v2": "/tmp/lektrix/site/test/img/lex_pastmonths",
-    "year_graph_v2": "/tmp/lektrix/site/test/img/lex_pastyears",
-    "yg_vs_month_v2": "/tmp/lektrix/site/test/img/lex_vs_month",
-    "yg_gauge_v2": "/tmp/lektrix/site/test/img/lex_gauge",
+    "hour_graph_v2": "/tmp/lektrix/site//img/lex_pasthours",
+    "day_graph_v2": "/tmp/lektrix/site//img/lex_pastdays",
+    "month_graph_v2": "/tmp/lektrix/site//img/lex_pastmonths",
+    "year_graph_v2": "/tmp/lektrix/site//img/lex_pastyears",
+    "yg_vs_month_v2": "/tmp/lektrix/site//img/lex_vs_month",
+    "yg_gauge_v2": "/tmp/lektrix/site//img/lex_gauge",
 }
 
 KAMSTRUP = {
