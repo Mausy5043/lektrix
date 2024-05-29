3c6caf6ce12bb49308ac16be2bde3886734cd0a5
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-05-29 19:35:24 +0200

    warnings fixed

--- bin/report.py
+++ bin/report.py
@@ -4,16 +4,6 @@
 Using kamstrup data
 """
 
-import warnings
-
-# FutureWarning: The default value of numeric_only in DataFrameGroupBy.sum is deprecated.
-# In a future version, numeric_only will default to False. Either specify numeric_only or
-# select only columns which should be valid for the function.
-#   df = df.resample(f"{aggregation}", label=lbl).sum()
-# warnings.simplefilter(action="ignore", category=FutureWarning)
-# DeprecationWarning: Pyarrow will become a required dependency of pandas in the next major release of pandas (pandas 3.0)
-# warnings.simplefilter(action="ignore", category=DeprecationWarning)
-
 # pylint: disable=C0413
 import argparse
 import sqlite3 as s3
