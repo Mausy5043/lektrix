d0d0980007c3c69fa07688450dc9074cebf4ba6f
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-08-14 19:01:48 +0200

    unused imports

--- bin/libsolaredge.py
+++ bin/libsolaredge.py
@@ -5,11 +5,13 @@
 import datetime as dt
 import functools
 import time
-from itertools import tee
+
+# from itertools import tee
 
 import constants
 import dateutil.parser as dtparse
-import dateutil.rrule as dtrule
+
+# import dateutil.rrule as dtrule
 import pandas as pd
 import pytz
 import requests
