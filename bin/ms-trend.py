5b1599e5a3187a2c32a2d8fe7969322f5edd44cd
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-10-19 09:00:00 +0200

    fix warnings

--- bin/ms-trend.py
+++ bin/ms-trend.py
@@ -142,7 +142,9 @@ def fetch_data_mains(hours_to_fetch=48, aggregation="H") -> pd.DataFrame:
 
     # Get the data
     with s3.connect(DATABASE) as con:
-        df = pd.read_sql_query(s3_query, con, parse_dates="sample_time", index_col="sample_epoch")
+        df = pd.read_sql_query(
+            s3_query, con, parse_dates=["sample_time"], index_col="sample_epoch"
+        )
     if DEBUG:
         print("o  database mains data")
         print(df)
@@ -207,7 +209,9 @@ def fetch_data_production(hours_to_fetch=48, aggregation="H") -> pd.DataFrame:
 
     # Get the data
     with s3.connect(DATABASE) as con:
-        df = pd.read_sql_query(s3_query, con, parse_dates="sample_time", index_col="sample_epoch")
+        df = pd.read_sql_query(
+            s3_query, con, parse_dates=["sample_time"], index_col="sample_epoch"
+        )
     if DEBUG:
         print("o  database production data")
         print(df)
@@ -298,7 +302,7 @@ def plot_graph(output_file, data_dict, plot_title, show_data=False, locatorforma
             if show_data:
                 x_offset = -0.1
                 for p in ax1.patches:
-                    b = p.get_bbox()
+                    b = p.get_bbox()  # type: ignore
                     val = f"{b.y1 - b.y0:{constants.FLOAT_FMT}}"
                     ax1.annotate(
                         val,
