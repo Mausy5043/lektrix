a840a5e9df787f891c0797d72eef01e861ecdffb
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-11-24 16:21:36 +0100

    not using requirements.txt (test)

--- bin/include.sh
+++ bin/include.sh
@@ -172,7 +172,7 @@ install_lektrix() {
     done
     # install Python3 stuff
     python3 -m pip install --upgrade pip setuptools wheel
-    python3 -m pip install --upgrade -r requirements.txt
+    python3 -m pip install --upgrade .
     echo
 
     # install account keys from local fileserver
