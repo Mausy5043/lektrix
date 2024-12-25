94576dc979b4c958df3121215ffff860e1fd52ab
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-12-25 10:08:38 +0100

    spinner is not working

--- bin/include.sh
+++ bin/include.sh
@@ -93,7 +93,8 @@ update_lektrix() {
     git fetch origin
     git checkout "${branch_name}"
     git reset --hard "origin/${branch_name}" && git clean -f -d
-    spinner python -m pip install --upgrade pip -r "${APPDIR}/requirements.txt" \
+    echo "pip update..."
+    python -m pip install --upgrade pip -r "${APPDIR}/requirements.txt" \
         | grep -v "Requirement already satisfied"
 }
 
