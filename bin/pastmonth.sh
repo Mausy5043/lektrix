c71fa616c11f4482911af2cdbf5b859aa20b34fb
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2024-12-26 11:28:34 +0100

    attempt to avoid/handle locked database

--- bin/pastmonth.sh
+++ bin/pastmonth.sh
@@ -4,10 +4,28 @@
 
 MAINTENANCE=${1}
 HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+max_retries=5
+retry_delay=32
 
 pushd "${HERE}" >/dev/null || exit 1
 # shellcheck disable=SC1091
 source ./include.sh
+
+execute_sql() {
+    local sql=$2
+    local database = $1
+    for ((i=1; i<=max_retries; i++)); do
+        if sqlite3 "${database}" "${sql}"; then
+            echo "SQL executed successfully: ${sql}"
+            return 0
+        else
+            echo "Database is locked. Retry $i/$max_retries in $retry_delay seconds..."
+            sleep $retry_delay
+        fi
+    done
+    echo "Failed to execute SQL after $max_retries attempts: ${sql}"
+    return 1
+}
 
 if [ "${MAINTENANCE}" == "-" ]; then
     # do some maintenance
@@ -15,7 +33,7 @@ if [ "${MAINTENANCE}" == "-" ]; then
 
     # shellcheck disable=SC2154
     echo "${db_full_path} re-indexing... "
-    sqlite3 "${db_full_path}" "REINDEX;"
+    execute_sql "${db_full_path}" "REINDEX;"
 
     echo -n "${db_full_path} integrity check:   "
     chk_result=$(sqlite3 "${db_full_path}" "PRAGMA integrity_check;")
@@ -33,12 +51,15 @@ if [ "${MAINTENANCE}" == "-" ]; then
         # Keep upto 10 years of data
         echo "${db_full_path} vacuuming... "
         PURGE_EPOCH=$(echo "${CURRENT_EPOCH} - (3660 * 24 * 3600)" | bc)
-        sqlite3 "${db_full_path}" \
-            "DELETE FROM mains WHERE sample_epoch < ${PURGE_EPOCH};"
-        sqlite3 "${db_full_path}" \
-            "DELETE FROM production WHERE sample_epoch < ${PURGE_EPOCH};"
-        sqlite3 "${db_full_path}" \
-            "DELETE FROM charger WHERE sample_epoch < ${PURGE_EPOCH};"
+        execute_sql "${db_full_path}" "DELETE FROM mains WHERE sample_epoch < ${PURGE_EPOCH};"
+        execute_sql "${db_full_path}" "DELETE FROM production WHERE sample_epoch < ${PURGE_EPOCH};"
+        execute_sql "${db_full_path}" "DELETE FROM charger WHERE sample_epoch < ${PURGE_EPOCH};"
+        # sqlite3 "${db_full_path}" \
+        #     "DELETE FROM mains WHERE sample_epoch < ${PURGE_EPOCH};"
+        # sqlite3 "${db_full_path}" \
+        #     "DELETE FROM production WHERE sample_epoch < ${PURGE_EPOCH};"
+        # sqlite3 "${db_full_path}" \
+        #     "DELETE FROM charger WHERE sample_epoch < ${PURGE_EPOCH};"
     fi
     # sync the database into the cloud
     if command -v rclone &> /dev/null; then
