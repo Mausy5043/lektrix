56403084aadfb0f4501fa287b61da7139a39f73e
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-03-22 12:19:36 +0100

    fix typo

--- bin/pastday.sh
+++ bin/pastday.sh
@@ -5,5 +5,5 @@ HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 pushd "${HERE}" >/dev/null || exit 1
     ./ms-trend.py --hours 0
     ./me-trend.py --hours 0
-    ./pricetrend.py --twodays
+    ./pricetrend.py --twoday
 popd >/dev/null || exit
