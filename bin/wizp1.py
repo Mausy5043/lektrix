594525a49083c914caf56841066ae75d25564ab4
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-03-25 22:04:35 +0100

    use @homewizard API/v2 for data gathering

--- bin/wizp1.py
+++ bin/wizp1.py
@@ -67,7 +67,7 @@ def main() -> None:
     LOGGER.info(f"Running on Python {sys.version}")
     set_led("mains", "orange")
     killer = gk.GracefulKiller()
-    API_P1 = p1.WizP1_v1(debug=DEBUG)
+    API_P1 = p1.WizP1_v2(debug=DEBUG)
 
     sql_db = m3.SqlDatabase(
         database=constants.WIZ_P1["database"],
