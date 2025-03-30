d969c5108765cfd77d902f012c378bd2e56dd4bd
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-03-30 12:02:54 +0200

    file renamed

--- bin/pastday.sh
+++ bin/pastday.sh
@@ -5,5 +5,5 @@ HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 pushd "${HERE}" >/dev/null || exit 1
     ./ms-trend.py --hours 0
     ./me-trend.py --hours 0
-    ./pricetrend.py --twoday
+    ./trendprice.py --twoday
 popd >/dev/null || exit
