59c0068d096283cb54ae840e978a1c88ba51a248
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-04-04 10:01:49 +0200

    this is always a dict

--- bin/prices.py
+++ bin/prices.py
@@ -32,7 +32,7 @@ def req_post(
             verify=False,  # nosec B501
         )
         response.raise_for_status()  # Raise an exception for HTTP errors
-        return response.json()
+        return dict(response.json())
     except requests.exceptions.RequestException as her:
         print(f"An error occurred: {her}")
         return {}
