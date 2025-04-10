6e60c2375b0b7b1ef7a50ced240432ab617df4c4
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-04-10 21:47:23 +0200

    force formatting

--- bin/libwizkwh.py
+++ bin/libwizkwh.py
@@ -112,19 +112,11 @@ class WizkWh:
         LOGGER.debug(f"PV:\n{_ev}")
         _p1 = telegram[2]
         LOGGER.debug(f"P1:\n{_ev}")
-        self.ev_elec_in = int(
-            _ev.energy_export_kwh * 1000
-        )  # EV kWH-meter is connected wrong way round
-        self.pv_elec_in = int(
-            _pv.energy_export_kwh * 1000
-        )  # PV kWH-meter is connected wrong way round
+        self.ev_elec_in = int(_ev.energy_export_kwh * 1000)  # EV kWH-meter is connected wrong way round
+        self.pv_elec_in = int(_pv.energy_export_kwh * 1000)  # PV kWH-meter is connected wrong way round
         self.p1_elec_in = int(_p1.energy_import_kwh * 1000)
-        self.ev_elec_out = int(
-            _ev.energy_import_kwh * -1000
-        )  # EV kWH-meter is connected wrong way round
-        self.pv_elec_out = int(
-            _pv.energy_import_kwh * -1000
-        )  # PV kWH-meter is connected wrong way round
+        self.ev_elec_out = int(_ev.energy_import_kwh * -1000)  # EV kWH-meter is connected wrong way round
+        self.pv_elec_out = int(_pv.energy_import_kwh * -1000)  # PV kWH-meter is connected wrong way round
         self.p1_elec_out = int(_p1.energy_export_kwh * -1000)
         self.ev_voltage = _ev.voltage_v
         self.pv_voltage = _pv.voltage_v
