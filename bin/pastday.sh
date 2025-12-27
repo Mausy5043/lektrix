55911089d55eade681f3ea2ae0e5be7cf8896496
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-12-27 15:34:07 +0100

    consolidate trend creation

--- bin/pastday.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
-
-pushd "${HERE}" >/dev/null || exit 1
-    ./trendhw.py --hours 0
-    ./trendprice.py --twoday
-popd >/dev/null || exit
