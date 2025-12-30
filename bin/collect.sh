11b3f923e8a3be7d8dfaf049a9604ec16e227c7e
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-12-30 16:35:25 +0100

    initial commit

---
+++ bin/collect.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+TEST="${1}"
+HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)  # /app/scripts
+
+echo "Collecting data..."
+pushd "${HERE}" >/dev/null || exit 1
+    echo "TEST = ${TEST}"
+popd >/dev/null || exit 1
+echo "Data collection done."
