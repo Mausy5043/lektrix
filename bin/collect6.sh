281aa97bfe6a408290a3e9c34e57d9a091e62352
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2026-02-28 20:44:46 +0100

    new scripts

---
+++ bin/collect6.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)  # /app/scripts
+
+echo "Collecting data..."
+
+pushd "${HERE}" >/dev/null || exit 1
+    # /app/scripts/sessy.py --single &
+    /app/scripts/solaredge-v1.py --single &
+    # /app/scripts/wizkwh.py --single &
+    wait
+popd >/dev/null || exit 1
+
+echo "Data collection done."
