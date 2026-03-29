cc53d64a1c1941776b61a51f61b4b85d59909720
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2026-03-29 14:17:28 +0200

    remove legacy code

--- bin/monthly.sh
+++ bin/monthly.sh
@@ -2,13 +2,8 @@
 
 HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 
-# shellcheck disable=SC1091
-source /home/pi/.pyenvpaths
-
 pushd "${HERE}" >/dev/null || exit 1
     ./trendhw.py --months 6 --debug > /tmp/report.txt
 popd >/dev/null || exit
 
 last_month=$(date -d "$(date +%Y-%m-15) -1 month" +%m-%Y)
-
-/home/pi/bin/pymail.py -s "electricity report for ${last_month}" -f /tmp/report.txt
