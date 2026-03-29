235c4e3b00b91475d4b68d12aa4f285c688ce897
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2026-03-29 14:32:26 +0200

    don;t use /tmp

--- bin/monthly.sh
+++ bin/monthly.sh
@@ -2,8 +2,7 @@
 
 HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 
-pushd "${HERE}" >/dev/null || exit 1
-    ./trendhw.py --months 6 --debug > /tmp/report.txt
-popd >/dev/null || exit
-
 last_month=$(date -d "$(date +%Y-%m-15) -1 month" +%m-%Y)
+pushd "${HERE}" >/dev/null || exit 1
+    ./trendhw.py --months 6 --debug > "../data/hw-report_${last_month}.txt"
+popd >/dev/null || exit
